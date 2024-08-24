e86ecc9f5c29eb7822cc065fd155919d6b85ff3a
Yi Li Chen <44828678+yilichen89@users.noreply.github.com>
2024-08-24 11:06:22 +0800

    Create hangxin.js

---
+++ Rule/hangxin.js
@@ -0,0 +1,45 @@
+[mitm]
+hostname = cloud.bankofchina.com
+[rewrite_local]
+^https:\/\/cloud\.bankofchina\.com\/zj\/pigeonqy\/signinmgr\/recordowrkndoffwork.php url script-response-body https://raw.githubusercontent.com/yilichen89/Scripts/main/Rule/hangxin.js
+^https:\/\/cloud\.bankofchina\.com\/zj\/pigeonqy\/signinmgr\/recordowrkndoffwork.php url script-request-body https://raw.githubusercontent.com/yilichen89/SC/main/Rule/hangxin.js
+/**********************************************************/
+// Quantumult X Script
+
+const url = $request.url;
+const targetURL = "https://cloud.bankofchina.com/zj/pigeonqy/signinmgr/attencelocation.php";
+
+if (url.indexOf(targetURL) !== -1) {
+    // 处理请求体
+    var body = $request.body;
+    if (body) {
+        body = body.replace(/actuallongitude=[^&]*/, "actuallongitude=120.585252");
+        body = body.replace(/actuallatitude=[^&]*/, "actuallatitude=29.996876");
+        body = body.replace(/ctualscope=[^&]*/, "ctualscope=27.8");
+    }
+
+    // 处理响应体
+    let responseBody = $response.body;
+    
+    // 替换 .hide() 为 .show()
+    responseBody = responseBody.replace(/\.hide\(\)/g, '.show()');
+    
+    // 完全移除检查距离和显示提示的相关代码
+    responseBody = responseBody.replace(/if\s*\(\s*signscope\s*<\s*distance\s*\)\s*\{[\s\S]*?showtips\s*\([^)]*\)\s*;[\s\S]*?\}/g, '');
+    
+    // 修改签到和签退函数，移除距离检查逻辑
+    responseBody = responseBody.replace(/function\s+clockin\(\)\s*\{[\s\S]*?\}/g, function(match) {
+        return match.replace(/if\s*\(\s*signscope\s*<\s*distance\s*\)\s*\{[\s\S]*?\}/g, '');
+    });
+    
+    responseBody = responseBody.replace(/function\s+signout\(\)\s*\{[\s\S]*?\}/g, function(match) {
+        return match.replace(/if\s*\(\s*signscope\s*<\s*distance\s*\)\s*\{[\s\S]*?\}/g, '');
+    });
+    
+    // 修改if语句的条件
+    responseBody = responseBody.replace(/if\s*\(\s*signscope\s*>=\s*distance\s*\)/g, 'if(true)');
+    
+    $done({body: body, response: {body: responseBody}});
+} else {
+    $done({});
+}
