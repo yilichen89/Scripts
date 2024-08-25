655738f636d1a112630acc5567ce83d9a8498e9f
Yi Li Chen <44828678+yilichen89@users.noreply.github.com>
2024-08-25 15:50:31 +0800

    Update hangxin.js

--- Rule/hangxin.js
+++ Rule/hangxin.js
@@ -7,41 +7,31 @@ hostname = cloud.bankofchina.com
 // Quantumult X Script
 
 const url = $request.url;
+const targetURLs = [
+    "https://cloud.bankofchina.com/zj/pigeonqy/signinmgr/attencelocation.php",
+    "https://cloud.bankofchina.com/zj/pigeonqy/signinmgr/recordworkandoffwork.php"
+];
 
-if (url.includes("https://cloud.bankofchina.com/zj/pigeonqy/signinmgr/recordowrkndoffwork.php")) {
-    // 处理位置信息请求
-    var body = $request.body;
-    if (body) {
+if (targetURLs.some(targetURL => url.indexOf(targetURL) !== -1)) {
+    let body = $request.body || $response.body; // 判断是请求还是响应
+
+    // 对于 attencelocation.php
+    if (url.indexOf("attencelocation.php") !== -1) {
+        // 修改响应内容
+        body = body.replace(/\.hide\(\)/g, '.show()');
+        body = body.replace(/if\(signscope < distance\)\{[\s\S]*?showtips\(2,"温馨提示","超出考勤范围"\);\s*\}/, '');
+        body = body.replace(/if\(signscope >= distance\)/g, 'if(distance)');
+    }
+
+    // 对于 recordworkandoffwork.php
+    if (url.indexOf("recordworkandoffwork.php") !== -1) {
+        // 修改请求体
         body = body.replace(/actuallongitude=[^&]*/, "actuallongitude=120.585252");
         body = body.replace(/actuallatitude=[^&]*/, "actuallatitude=29.996876");
         body = body.replace(/ctualscope=[^&]*/, "ctualscope=27.8");
     }
-    $done({body: body});
-}
-else if (url.includes("https://cloud.bankofchina.com/zj/pigeonqy/signinmgr/attencelocation.php")) {
-    // 处理响应页面
-    let responseBody = $response.body;
-    
-    // 替换 .hide() 为 .show()
-    responseBody = responseBody.replace(/\.hide\(\)/g, '.show()');
-    
-    // 完全移除检查距离和显示提示的相关代码
-    responseBody = responseBody.replace(/if\s*\(\s*signscope\s*<\s*distance\s*\)\s*\{[\s\S]*?showtips\s*\([^)]*\)\s*;[\s\S]*?\}/g, '');
-    
-    // 修改签到和签退函数，移除距离检查逻辑
-    responseBody = responseBody.replace(/function\s+clockin\(\)\s*\{[\s\S]*?\}/g, function(match) {
-        return match.replace(/if\s*\(\s*signscope\s*<\s*distance\s*\)\s*\{[\s\S]*?\}/g, '');
-    });
-    
-    responseBody = responseBody.replace(/function\s+signout\(\)\s*\{[\s\S]*?\}/g, function(match) {
-        return match.replace(/if\s*\(\s*signscope\s*<\s*distance\s*\)\s*\{[\s\S]*?\}/g, '');
-    });
-    
-    // 修改if语句的条件
-    responseBody = responseBody.replace(/if\s*\(\s*signscope\s*>=\s*distance\s*\)/g, 'if(true)');
-    
-    $done({body: responseBody});
-}
-else {
+
+    $done({body});
+} else {
     $done({});
 }
