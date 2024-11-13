719a738f97f535971cff6eb51fd6cc31d8f5ef7c
Yi Li Chen <44828678+yilichen89@users.noreply.github.com>
2024-11-13 21:52:17 +0800

    Update modify-location.js

--- hangxin/modify-location.js
+++ hangxin/modify-location.js
@@ -1,7 +1,7 @@
-let body = req.body;
+def("body", $request.body);
 if (body) {
-    body = body.replace(/actuallongitude=[^&]*/, "actuallongitude=120.585252");
-    body = body.replace(/actuallatitude=[^&]*/, "actuallatitude=29.996876");
-    body = body.replace(/ctualscope=[^&]*/, "ctualscope=27.8");
+    body = body | sub("actuallongitude=[^&]*", "actuallongitude=120.585252");
+    body = body | sub("actuallatitude=[^&]*", "actuallatitude=29.996876");
+    body = body | sub("ctualscope=[^&]*", "ctualscope=27.8");
 }
-$done({body});
+$done({body: body});
