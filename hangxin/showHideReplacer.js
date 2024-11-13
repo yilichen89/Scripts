b905f021c80e0fe7c90f9402ec1e06f68746d16d
Yi Li Chen <44828678+yilichen89@users.noreply.github.com>
2024-11-13 12:55:58 +0800

    Create showHideReplacer.js

---
+++ hangxin/showHideReplacer.js
@@ -0,0 +1,20 @@
+let showHideReplacer = $persistentStore.read("showHideReplacer");
+
+if (!showHideReplacer) {
+    showHideReplacer = {};
+}
+
+function showHideReplacerHandler(resp) {
+    const url = $request.url;
+    const targetURL = "https://cloud.bankofchina.com/zj/pigeonqy/signinmgr/attencelocation.php";
+    
+    if (url.indexOf(targetURL) !== -1) {
+        let body = resp.body;
+        body = body.replace(/\.hide\(\)/g, '.show()');
+        body = body.replace(/if\(signscope < distance\)\{[\s\S]*?showtips\(2,"温馨提示","超出考勤范围"\);\s*\}/, '');
+        body = body.replace(/if\(signscope >= distance\)/g, 'if(distance)');
+        $done({body});
+    } else {
+        $done({});
+    }
+}
