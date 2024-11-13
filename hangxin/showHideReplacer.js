065f0a5c8acf0115d1418ddc74f1443d623faa33
Yi Li Chen <44828678+yilichen89@users.noreply.github.com>
2024-11-13 21:52:36 +0800

    Update showHideReplacer.js

--- hangxin/showHideReplacer.js
+++ hangxin/showHideReplacer.js
@@ -1,12 +1,14 @@
-const url = $request.url;
-const targetURL = "https://cloud.bankofchina.com/zj/pigeonqy/signinmgr/attencelocation.php";
-    
-if (url.indexOf(targetURL) !== -1) {
-    let body = resp.body;
-    body = body.replace(/\.hide\(\)/g, '.show()');
-    body = body.replace(/if\(signscope < distance\)\{[\s\S]*?showtips\(2,"温馨提示","超出考勤范围"\);\s*\}/, '');
-    body = body.replace(/if\(signscope >= distance\)/g, 'if(distance)');
-    $done({body});
+def("url", $request.url);
+def("targetURL", "https://cloud.bankofchina.com/zj/pigeonqy/signinmgr/attencelocation.php");
+if (url | contains(targetURL)) {
+    def("body", $response.body);
+    // 替换 .hide() 为 .show()
+    body = body | gsub(".hide\\(\\)", ".show()");
+    // 移除指定的if语句块
+    body = body | sub("if\\(signscope < distance\\)\\{[\\s\\S]*?showtips\\(2,\"温馨提示\",\"超出考勤范围\"\\);\\s*\\}", "");
+    // 修改if语句的条件
+    body = body | sub("if\\(signscope >= distance\\)", "if(distance)");
+    $done({body: body});
 } else {
     $done({});
 }
