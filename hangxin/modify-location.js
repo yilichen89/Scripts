6c4f34ac6aafb81886d370eb846f98aefc9ac1b5
Yi Li Chen <44828678+yilichen89@users.noreply.github.com>
2024-11-13 13:21:03 +0800

    Update modify-location.js

--- hangxin/modify-location.js
+++ hangxin/modify-location.js
@@ -1,16 +1,7 @@
-# modify-location.js
-let modifyLocation = $persistentStore.read("modifyLocation");
-
-if (!modifyLocation) {
-    modifyLocation = {};
+let body = req.body;
+if (body) {
+    body = body.replace(/actuallongitude=[^&]*/, "actuallongitude=120.585252");
+    body = body.replace(/actuallatitude=[^&]*/, "actuallatitude=29.996876");
+    body = body.replace(/ctualscope=[^&]*/, "ctualscope=27.8");
 }
-
-function modifyLocationHandler(req) {
-    let body = req.body;
-    if (body) {
-        body = body.replace(/actuallongitude=[^&]*/, "actuallongitude=120.585252");
-        body = body.replace(/actuallatitude=[^&]*/, "actuallatitude=29.996876");
-        body = body.replace(/ctualscope=[^&]*/, "ctualscope=27.8");
-    }
-    $done({body});
-}
+$done({body});
