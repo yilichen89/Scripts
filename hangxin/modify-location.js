fd5d067a857e3040a7689d9322b340b202f78eab
Yi Li Chen <44828678+yilichen89@users.noreply.github.com>
2025-12-23 22:35:01 +0800

    Update modify-location.js

--- hangxin/modify-location.js
+++ hangxin/modify-location.js
@@ -1,10 +1,10 @@
-let body = $response.body;
+let body = $request.body;
 if (body) {
     let obj = JSON.parse(body);
     if (obj.data) {
-        obj.data.longitude1 = 120.5853; 
-        obj.data.latitude1 = 29.99663;
-        obj.data.signScope = "27.8";
+        obj.data.actualLatitude = 29.99656480435857; 
+        obj.data.actualLongitude = 120.5850024356243;
+        obj.data.ctualScope = "29.6";
     }
     $done({ body: JSON.stringify(obj) });
 }
